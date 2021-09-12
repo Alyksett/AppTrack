ecb8cd61fb1ee06f777e8163e2ce4012a6150abe
Alyksett <86544889+Alyksett@users.noreply.github.com>
2021-09-11 20:13:05 -0700

    Added documentation

--- EditApplicationGUI.java
+++ EditApplicationGUI.java
@@ -1,12 +1,18 @@
+/**
+ * This is the class for the second window that allows the user to adjust the contents of any
+ * of the applications in the database. 
+ * 
+ * Author: Austin Lyksett
+ * 9/11/2021
+ */
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 class EditApplicationGUI extends JFrame {
-
   private JButton saveChangesButton  = new JButton("Save Changes");
-
-
   private JTextField companyNameField = new JTextField(" ");
 
   private JTextField applyDateField = new JTextField(" ");
@@ -15,7 +21,7 @@ class EditApplicationGUI extends JFrame {
   private JTextArea notesField = new JTextArea(" ");
   private static JComboBox namePicker;
   
-
+  //constructor
   public EditApplicationGUI(){
     setTitle("AppTrack");
     setSize(350,250);
@@ -30,9 +36,7 @@ class EditApplicationGUI extends JFrame {
   }
 
   private void initComponent(){
-    saveChangesButton.setBounds(205,180, 125,25);
-
-    
+    saveChangesButton.setBounds(205,180, 125,25);  
     namePicker.setBounds(170, 5, 150, 25);
 
     companyNameField.setBounds(20, 5, 145, 20);
@@ -40,7 +44,6 @@ class EditApplicationGUI extends JFrame {
     OAField.setBounds(20, 55, 145, 20);
     rejectField.setBounds(20, 80, 145, 20);
     notesField.setBounds(20, 105, 145, 100);
-
 
     add(saveChangesButton);
     add(namePicker);
@@ -51,14 +54,12 @@ class EditApplicationGUI extends JFrame {
     add(OAField);
     add(rejectField);
     add(notesField);
-
     }
-
+    //event handling
   private void initEvent(){
-
-    // for when the item in the combobox is highlighted
-    namePicker.addItemListener(new ItemListener(){
-        @Override
+    // for when the item in the combobox is highlighted, it adjusts all the text fields to the selected item. 
+    namePicker.addItemListener(new ItemListener(){  
+      @Override
         public void itemStateChanged(ItemEvent e){
             if(e.getStateChange() == ItemEvent.SELECTED) {
                 companyNameField.setText(Driver.database.get(namePicker.getSelectedItem()).getComanyName());
@@ -69,13 +70,13 @@ class EditApplicationGUI extends JFrame {
             }
         }
     });
-
+    //this method is for saving the changes made to the application. It sets the text fields to local variables, then 
+    // it calls the updateApplication method in Driver, passing these variables.
     saveChangesButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {    
         String name = companyNameField.getText();
         String date = applyDateField.getText();
-        String OA = OAField.getText();
-        System.out.println(OA + "This is the OA field from the GUI");        
+        String OA = OAField.getText();    
         String rejected = rejectField.getText();
         String notes = notesField.getText();
         Driver.updateApplication(name, date, OA, rejected, notes);
